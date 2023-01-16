16dfa38efb9363e42e34f2343e51b5e599719dac
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-01-16 12:31:15 +0800

    install script

--- install.sh
+++ install.sh
@@ -1,1 +1,17 @@
 #!/bin/zsh
+
+echo "Installing homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install gh
+echo "Logging in with github"
+gh auth login
+
+echo "Installing Brew Bundle"
+brew bundle --file="./brewfile"
+
+echo "Stowing configs"
+stow -vR */
+
+echo "Applying OSX settings"
+bash osx_settings
