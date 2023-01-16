d6c7acd26e3cbe3cc4b4f51eafebae50abee217b
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-01-16 09:44:14 +0800

    toggle jabs mapping

--- nvim/.config/nvim/lua/plugins/jabs.lua
+++ nvim/.config/nvim/lua/plugins/jabs.lua
@@ -1,10 +1,29 @@
+local function toggleJABS()
+  local j = require("jabs")
+  if j.main_buf then
+    j.close()
+  else
+    j.open()
+  end
+end
+
 local M = {
   "matbme/JABS.nvim",
   keys = {
-    { "<leader>b", ":JABSOpen<CR>" },
+    {
+      "<leader>b",
+      function()
+        toggleJABS()
+      end,
+      desc = "toggle JABS window",
+    },
   },
   opts = {
     position = { "center", "center" },
+    border = "none",
+    preview = {
+      border = "none",
+    },
   },
 }
 
