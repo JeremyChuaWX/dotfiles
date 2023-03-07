cfb2889fef54979a31ed3d5eb7326852af7fd37a
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-03-07 13:30:16 +0800

    change sumneko_lua to lua_ls

--- nvim/.config/nvim/lua/lsp/servers/init.lua
+++ nvim/.config/nvim/lua/lsp/servers/init.lua
@@ -3,7 +3,7 @@ local M = {}
 M.jsonls = require("lsp.servers.jsonls")
 M.solc = require("lsp.servers.solc")
 M.solidity_ls = require("lsp.servers.solidity_ls")
-M.sumneko_lua = require("lsp.servers.sumneko_lua")
+M.lua_ls = require("lsp.servers.lua_ls")
 M.tailwindcss = require("lsp.servers.tailwindcss")
 
 return M
