22ada4d05cf55155cd309465e992fdee8a91d768
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-03-11 16:17:49 +0800

    cmp dependencies and lazy loading

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,6 +1,15 @@
 local M = {
   "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
+  event = { "InsertEnter", "CmdlineEnter" },
+  dependencies = {
+    "tzachar/cmp-fuzzy-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-cmdline",
+    "saadparwaiz1/cmp_luasnip",
+    "hrsh7th/cmp-nvim-lsp",
+    "onsails/lspkind.nvim",
+    "L3MON4D3/LuaSnip",
+  },
 }
 
 M.config = function()
