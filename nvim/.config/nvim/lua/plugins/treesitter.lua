f013216fa4703194fa2fa83d8aec4283c7180a48
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-04-08 14:04:18 +0800

    foldexpr

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -31,7 +31,7 @@ M.config = function()
   })
 
   vim.opt.foldmethod = "expr"
-  vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+  vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 end
 
 return M
