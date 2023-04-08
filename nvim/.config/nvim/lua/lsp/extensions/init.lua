380a310fabeba333e3033fa6fbd81e210bcd75d7
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-04-08 09:29:34 +0800

    reenable fidget

--- nvim/.config/nvim/lua/lsp/extensions/init.lua
+++ nvim/.config/nvim/lua/lsp/extensions/init.lua
@@ -1,4 +1,4 @@
 require("lsp.extensions.null-ls")
 -- require("lsp.extensions.lsp-signature")
--- require("lsp.extensions.fidget")
+require("lsp.extensions.fidget")
 require("lsp.extensions.neodev")
