1af9287854c0c6c8a573a3973eb8256015008491
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-04-14 21:57:20 +0800

    add dressing

--- nvim/.config/nvim/lua/plugins/noconfig.lua
+++ nvim/.config/nvim/lua/plugins/noconfig.lua
@@ -2,11 +2,9 @@ return {
   "nvim-lua/popup.nvim",
   "nvim-lua/plenary.nvim",
   "neovim/nvim-lspconfig",
-  -- "williamboman/mason.nvim",
-  -- "williamboman/mason-lspconfig.nvim",
-  -- "WhoIsSethDaniel/mason-tool-installer.nvim",
   "mfussenegger/nvim-jdtls",
   "simrat39/rust-tools.nvim",
   "jose-elias-alvarez/typescript.nvim",
   "b0o/schemastore.nvim",
+  "stevearc/dressing.nvim",
 }
