c98229e32e37fac2f68a786bebcdafeacf3b8052
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-04-23 10:03:53 +0800

    rust analyzer settings

--- nvim/.config/nvim/lua/lsp/servers/init.lua
+++ nvim/.config/nvim/lua/lsp/servers/init.lua
@@ -5,5 +5,6 @@ M.solc = require("lsp.servers.solc")
 M.solidity_ls = require("lsp.servers.solidity_ls")
 M.lua_ls = require("lsp.servers.lua_ls")
 M.tailwindcss = require("lsp.servers.tailwindcss")
+M.rust_analyzer = require("lsp.servers.rust_analyzer")
 
 return M
