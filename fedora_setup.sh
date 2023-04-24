d8a785a5afcb9a81fc061eb2531bb9419a570bbb
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-04-24 11:29:28 +0800

    separate setup scripts

---
+++ fedora_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+# speed up dnf
+echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf
+echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
+
+# better fonts
+sudo dnf copr enable dawid/better_fonts -y
+sudo dnf install fontconfig-font-replacements -y --skip-broken
+sudo dnf install fontconfig-enhanced-defaults -y --skip-broken
+
+# tiling windows
+sudo dnf install gnome-shell-extension-pop-shell xprop
