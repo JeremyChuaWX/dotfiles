5a913fc91c663027d0a408ce9b012f3ab6fd5690
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-04-30 12:39:35 +0800

    setup scripts

--- fedora/fedora_setup.sh
+++ fedora/fedora_setup.sh
@@ -17,17 +17,12 @@ sudo dnf copr enable dawid/better_fonts -y
 sudo dnf install fontconfig-font-replacements -y --skip-broken
 sudo dnf install fontconfig-enhanced-defaults -y --skip-broken
 
+# stuff i use
+zsh ./packages
+
 # gnome extensions and tweaks
 sudo dnf install -y gnome-extensions-app gnome-tweaks
 
 # pop shell install
 # sudo dnf install -y gnome-shell-extension-pop-shell
 # then logout, login, activate in extensions
-
-# pop shell keymaps
-# sudo dnf install -y make cargo rust gtk3-devel
-# git clone https://github.com/pop-os/shell-shortcuts /home/$USER/fedora/pop-theme/shell-shortcuts
-# cd /home/$USER/fedora/pop-theme/shell-shortcuts
-# make
-# sudo make install
-# pop-shell-shortcuts
