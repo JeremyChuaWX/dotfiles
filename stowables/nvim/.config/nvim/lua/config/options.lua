a08841d0fe6b4147860432e16be5bb8bbb0aadcf
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-04-30 11:55:25 +0800

    indentation options

--- stowables/nvim/.config/nvim/lua/config/options.lua
+++ stowables/nvim/.config/nvim/lua/config/options.lua
@@ -110,9 +110,9 @@ opt.autoread = true
 
 opt.expandtab = true
 opt.smarttab = true
-opt.tabstop = 2
-opt.softtabstop = 2
-opt.shiftwidth = 2
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
 opt.autoindent = true
 opt.smartindent = true
 
