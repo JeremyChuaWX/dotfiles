fadb22c99c04b4f8b9d952f88ceb86045baed8b8
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-04-30 11:27:21 +0800

    fix merge stuff

--- macos/macos_setup.sh
+++ macos/macos_setup.sh
@@ -3,18 +3,19 @@
 echo "Installing homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install gh
-echo "Logging in with github"
-gh auth login
-
 echo "Installing brew bundle"
 brew bundle --file="./brewfile"
+
+echo "Logging in with github"
+gh auth login
 
 echo "Installing npm packages"
 zsh npmfile
 
 echo "Stowing configs"
-stow -vR */
+cd $HOME/.dotfiles/stowables
+stow -vR */ -t ~
+cd $HOME/.dotfiles/macos
 
 echo "Applying OSX settings"
 zsh osx_settings
