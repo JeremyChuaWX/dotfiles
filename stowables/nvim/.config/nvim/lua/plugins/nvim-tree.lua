443dd85712dc6e6306c29e2ac9fdc35ad2cf24d6
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-05-02 09:26:53 +0800

    add nvimtree

---
+++ stowables/nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,30 @@
+local M = {
+    "nvim-tree/nvim-tree.lua",
+    keys = {
+        { "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "nvim-tree" },
+    },
+    opts = {
+        view = {
+            width = 50,
+        },
+        renderer = {
+            add_trailing = true,
+            icons = {
+                git_placement = "signcolumn",
+            },
+        },
+        diagnostics = {
+            enable = true,
+            show_on_open_dirs = false,
+        },
+        git = {
+            show_on_open_dirs = false,
+        },
+        modified = {
+            enable = true,
+            show_on_open_dirs = false,
+        },
+    },
+}
+
+return M
