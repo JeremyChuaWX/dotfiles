9783102bb8b84e540d4795b65424ee358237edb9
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-05-02 09:26:31 +0800

    use tabspacing 4

--- stowables/nvim/.config/nvim/lua/plugins/null-ls.lua
+++ stowables/nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -1,42 +1,42 @@
 local M = {
-  "jose-elias-alvarez/null-ls.nvim",
+    "jose-elias-alvarez/null-ls.nvim",
 }
 
 M.config = function()
-  local null_ls = require("null-ls")
-  local formatting = null_ls.builtins.formatting
-  local diagnostics = null_ls.builtins.diagnostics
-  local actions = null_ls.builtins.code_actions
+    local null_ls = require("null-ls")
+    local formatting = null_ls.builtins.formatting
+    local diagnostics = null_ls.builtins.diagnostics
+    local actions = null_ls.builtins.code_actions
 
-  null_ls.setup({
-    debug = false,
-    diagnostics_format = "[#{c}] #{m} (#{s})",
-    update_in_insert = true,
-    on_attach = require("lsp.server-config").on_attach,
-    sources = {
-      actions.gitsigns,
-      formatting.prettierd,
-      formatting.rustywind,
-      formatting.rustfmt,
-      formatting.gofumpt,
-      formatting.stylua.with({
-        extra_args = {
-          "--indent-type",
-          "Spaces",
-          "--indent-width",
-          "2",
+    null_ls.setup({
+        debug = false,
+        diagnostics_format = "[#{c}] #{m} (#{s})",
+        update_in_insert = true,
+        on_attach = require("lsp.server-config").on_attach,
+        sources = {
+            actions.gitsigns,
+            formatting.prettierd,
+            formatting.rustywind,
+            formatting.rustfmt,
+            formatting.gofumpt,
+            formatting.stylua.with({
+                extra_args = {
+                    "--indent-type",
+                    "Spaces",
+                    "--indent-width",
+                    "4",
+                },
+            }),
+            formatting.beautysh.with({
+                extra_args = {
+                    "--indent-size",
+                    "4",
+                },
+            }),
+            diagnostics.actionlint,
+            diagnostics.markdownlint,
         },
-      }),
-      formatting.beautysh.with({
-        extra_args = {
-          "--indent-size",
-          "2",
-        },
-      }),
-      diagnostics.actionlint,
-      diagnostics.markdownlint,
-    },
-  })
+    })
 end
 
 return M
