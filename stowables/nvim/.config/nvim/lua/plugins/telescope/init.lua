5128661f053f698cabce9e040992f500328e5f1c
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-06-25 15:16:34 +0800

    remove default options

--- stowables/nvim/.config/nvim/lua/plugins/telescope/init.lua
+++ stowables/nvim/.config/nvim/lua/plugins/telescope/init.lua
@@ -73,13 +73,6 @@ M.config = function()
         },
 
         extensions = {
-            fzf = {
-                fuzzy = true, -- false will only do exact matching
-                override_generic_sorter = true, -- override the generic sorter
-                override_file_sorter = true, -- override the file sorter
-                case_mode = "smart_case", -- or "ignore_case" or "respect_case", default is "smart_case"
-            },
-
             tailiscope = {
                 register = '"',
                 default = "classes",
