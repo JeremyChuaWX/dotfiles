47e6fb84fe9eb6b5b117010df4b2ae98b9091651
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-09-30 12:41:18 +0800

    indent line migrate to v3

--- stowables/nvim/.config/nvim/lua/plugins/indentblankline.lua
+++ stowables/nvim/.config/nvim/lua/plugins/indentblankline.lua
@@ -1,35 +1,20 @@
 local M = {
     "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
     event = "VeryLazy",
+    opts = {
+        indent = {
+            char = "┃",
+        },
+        whitespace = {
+            remove_blankline_trail = true,
+        },
+        scope = {
+            show_start = false,
+            show_end = false,
+            highlight = { "MatchParen" },
+        },
+    },
 }
-
-M.config = function()
-    require("indent_blankline").setup({
-        char = "┃",
-        show_current_context = true,
-        show_first_indent_level = true,
-        show_trailing_blackline_indent = false,
-        filetype_exclude = {
-            "NvimTree",
-            "help",
-            "neogit",
-            "terminal",
-            "lspinfo",
-            "markdown",
-            "txt",
-            "git",
-            "gitcommit",
-            "TelescopePrompt",
-            "", -- for buffers without a file type
-        },
-        buftype_exclude = {
-            "terminal",
-            "nofile",
-        },
-    })
-
-    vim.api.nvim_set_hl(0, "IndentBlanklineChar", { fg = "#30303e" })
-    vim.api.nvim_set_hl(0, "IndentBlanklineContextChar", { fg = "#ffbf00" })
-end
 
 return M
