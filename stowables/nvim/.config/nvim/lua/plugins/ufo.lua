ad25a669e4265ec4bbd628b7aeca98ca6db823dd
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-10-03 13:35:42 +0800

    remove event from ufo

--- stowables/nvim/.config/nvim/lua/plugins/ufo.lua
+++ stowables/nvim/.config/nvim/lua/plugins/ufo.lua
@@ -1,6 +1,5 @@
 local M = {
     "kevinhwang91/nvim-ufo",
-    event = "BufReadPost",
     dependencies = {
         "kevinhwang91/promise-async",
         "luukvbaal/statuscol.nvim",
@@ -8,10 +7,10 @@ local M = {
 }
 
 M.config = function()
-    vim.o.foldcolumn = "1"
-    vim.o.foldlevel = 99
-    vim.o.foldlevelstart = 99
-    vim.o.foldenable = true
+    vim.opt.foldcolumn = "1"
+    vim.opt.foldlevel = 99
+    vim.opt.foldlevelstart = 99
+    vim.opt.foldenable = true
     vim.opt.fillchars:append("fold: ,foldsep: ,foldopen:,foldclose:")
 
     vim.keymap.set("n", "zR", function()
