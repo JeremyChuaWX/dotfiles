b7465e16130dc41d6848425892dccb8ccbf691c8
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-10-08 23:26:26 +0800

    formatting

--- stowables/nvim/.config/nvim/ftdetect/sol.lua
+++ stowables/nvim/.config/nvim/ftdetect/sol.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  desc = "Set filetype for *.sol files",
-  pattern = "*.sol",
-  command = "setf solidity",
+    desc = "Set filetype for *.sol files",
+    pattern = "*.sol",
+    command = "setf solidity",
 })
