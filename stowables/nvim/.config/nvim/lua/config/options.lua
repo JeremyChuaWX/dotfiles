ee1f604faf2ea5843472aebfaa25d5176521140a
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-10-08 23:23:30 +0800

    more verbose

--- stowables/nvim/.config/nvim/lua/config/options.lua
+++ stowables/nvim/.config/nvim/lua/config/options.lua
@@ -1,45 +1,43 @@
-local opt = vim.opt
-
 -------------
 -- general --
 -------------
 
-opt.compatible = false
-opt.ttyfast = true
-opt.updatetime = 50
-opt.timeoutlen = 500
-opt.mouse = "a"
+vim.opt.compatible = false
+vim.opt.ttyfast = true
+vim.opt.updatetime = 50
+vim.opt.timeoutlen = 500
+vim.opt.mouse = "a"
 
 ----------------
 -- temp files --
 ----------------
 
-opt.backup = false
-opt.swapfile = false
+vim.opt.backup = false
+vim.opt.swapfile = false
 -- opt.undofile = true
 
 --------------------
 -- ":" completion --
 --------------------
 
-opt.wildmenu = true
-opt.wildignore:append({ ".git", ".DS_Store" })
-opt.wildignorecase = true
-opt.wildmode = { "list:longest", "list:full" }
+vim.opt.wildmenu = true
+vim.opt.wildignore:append({ ".git", ".DS_Store" })
+vim.opt.wildignorecase = true
+vim.opt.wildmode = { "list:longest", "list:full" }
 
 --------------------
 -- tab completion --
 --------------------
 
-opt.completeopt = { "menu", "menuone", "noselect" }
-opt.pumheight = 10
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+vim.opt.pumheight = 10
 
 ---------------------
 -- split behaviour --
 ---------------------
 
-opt.splitbelow = true
-opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
 
 ---------------
 -- interface --
@@ -47,18 +45,18 @@ opt.splitright = true
 
 -- anything outside the buffer
 
-opt.ruler = false
-opt.number = true
-opt.numberwidth = 3
-opt.showcmd = true
-opt.showmode = false
-opt.cmdheight = 1
-opt.laststatus = 3
-opt.showtabline = 0
-opt.visualbell = true
-opt.signcolumn = "yes:2"
-opt.shortmess:append("c")
-opt.fillchars:append({
+vim.opt.ruler = false
+vim.opt.number = true
+vim.opt.numberwidth = 3
+vim.opt.showcmd = true
+vim.opt.showmode = false
+vim.opt.cmdheight = 1
+vim.opt.laststatus = 3
+vim.opt.showtabline = 0
+vim.opt.visualbell = true
+vim.opt.signcolumn = "yes:2"
+vim.opt.shortmess:append("c")
+vim.opt.fillchars:append({
     eob = " ",
     horiz = "━",
     horizup = "┻",
@@ -75,20 +73,20 @@ opt.fillchars:append({
 
 -- anything inside the buffer
 
-opt.encoding = "utf-8"
-opt.wrap = false
-opt.linebreak = true
-opt.whichwrap = "b,s,<,>,[,]"
-opt.joinspaces = false
-opt.backspace = { "indent", "eol", "start" }
-opt.list = true
-opt.listchars = { tab = "  ", trail = "", eol = "" }
-opt.iskeyword:append("-")
-opt.cursorline = true
-opt.scrolloff = 10
-opt.sidescrolloff = 10
-opt.guicursor = ""
-opt.colorcolumn = "80"
+vim.opt.encoding = "utf-8"
+vim.opt.wrap = false
+vim.opt.linebreak = true
+vim.opt.whichwrap = "b,s,<,>,[,]"
+vim.opt.joinspaces = false
+vim.opt.backspace = { "indent", "eol", "start" }
+vim.opt.list = true
+vim.opt.listchars = { tab = "  ", trail = "", eol = "" }
+vim.opt.iskeyword:append("-")
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 10
+vim.opt.guicursor = ""
+vim.opt.colorcolumn = "80"
 
 -------------
 -- folding --
@@ -104,35 +102,35 @@ vim.opt.fillchars:append("fold: ,foldsep: ,foldopen:,foldclose:")
 -- buffer and tabs --
 ---------------------
 
-opt.hidden = true
-opt.autoread = true
+vim.opt.hidden = true
+vim.opt.autoread = true
 
 -----------------
 -- indentation --
 -----------------
 
-opt.expandtab = true
-opt.smarttab = true
-opt.shiftround = true
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.shiftwidth = 4
-opt.autoindent = true
-opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.shiftround = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.autoindent = true
+vim.opt.smartindent = true
 
 ----------------
 -- appearance --
 ----------------
 
-opt.background = "dark"
-opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.termguicolors = true
 
 ------------
 -- search --
 ------------
 
-opt.ignorecase = true
-opt.smartcase = true
-opt.hlsearch = true
-opt.incsearch = true
-opt.wrapscan = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.wrapscan = true
