d81d2b5620bce3675c84c87e834e10cd31798d8d
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-10-08 23:25:05 +0800

    remove solidity settings

--- stowables/nvim/.config/nvim/lua/lsp/servers/init.lua
+++ stowables/nvim/.config/nvim/lua/lsp/servers/init.lua
@@ -1,8 +1,6 @@
 local M = {}
 
 M.jsonls = require("lsp.servers.jsonls")
-M.solc = require("lsp.servers.solc")
-M.solidity_ls = require("lsp.servers.solidity_ls")
 M.lua_ls = require("lsp.servers.lua_ls")
 M.tailwindcss = require("lsp.servers.tailwindcss")
 M.rust_analyzer = require("lsp.servers.rust_analyzer")
