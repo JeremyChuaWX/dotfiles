36ddf29d3f1997fe504b90163bc44bd68ce9b7cc
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-11-14 08:55:59 +0800

    remove legacy tag

--- stowables/nvim/.config/nvim/lua/plugins/fidget.lua
+++ stowables/nvim/.config/nvim/lua/plugins/fidget.lua
@@ -1,17 +1,6 @@
 local M = {
     "j-hui/fidget.nvim",
-    tag = "legacy",
-    opts = {
-        text = {
-            spinner = "dots_negative",
-            done = "✔",
-        },
-        sources = {
-            ["null-ls"] = {
-                ignore = true,
-            },
-        },
-    },
+    opts = {},
 }
 
 return M
