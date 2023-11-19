8d726db272679a0a68bf99cddac669056e048ce2
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-11-19 18:07:31 +0800

    use rayx lsp sig, hopefully it isnt slow

--- stowables/nvim/.config/nvim/lua/plugins/lsp_signature.lua
+++ stowables/nvim/.config/nvim/lua/plugins/lsp_signature.lua
@@ -1,13 +1,10 @@
 local M = {
-    "JeremyChuaWX/lsp_signature.nvim",
+    "ray-x/lsp_signature.nvim",
     opts = {
         floating_window = false,
         hint_enable = true,
         hint_prefix = "",
         hint_scheme = "Comment",
-        hint_inline = function()
-            return false
-        end,
     },
 }
 
