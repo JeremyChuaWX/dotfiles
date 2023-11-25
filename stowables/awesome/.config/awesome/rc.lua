c729aef38597a571851b1f710c35da88c8170fec
Jeremy Chua <chua.jeremy.wx@gmail.com>
2023-11-25 16:51:07 +0800

    update for arch

--- stowables/awesome/.config/awesome/rc.lua
+++ stowables/awesome/.config/awesome/rc.lua
@@ -18,10 +18,6 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
 
--- Load Debian menu entries
-local debian = require("debian.menu")
-local has_fdo, freedesktop = pcall(require, "freedesktop")
-
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
 -- another config (This code will only ever execute for the fallback config)
@@ -111,23 +107,12 @@ myawesomemenu = {
     },
 }
 
-local menu_awesome = { "awesome", myawesomemenu, beautiful.awesome_icon }
-local menu_terminal = { "open terminal", terminal }
-
-if has_fdo then
-    mymainmenu = freedesktop.menu.build({
-        before = { menu_awesome },
-        after = { menu_terminal },
-    })
-else
-    mymainmenu = awful.menu({
-        items = {
-            menu_awesome,
-            { "Debian", debian.menu.Debian_menu.Debian },
-            menu_terminal,
-        },
-    })
-end
+mymainmenu = awful.menu({
+    items = {
+        { "awesome", myawesomemenu, beautiful.awesome_icon },
+        { "open terminal", terminal },
+    },
+})
 
 mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon, menu = mymainmenu })
 
@@ -376,10 +361,10 @@ globalkeys = gears.table.join(
     end, { description = "show the menubar", group = "launcher" }),
 
     -- {{my keybinds
-    -- open firefox
+    -- open chrome
     awful.key({ modkey }, "b", function()
-        awful.spawn("firefox")
-    end, { description = "open firefox", group = "launcher" }),
+        awful.spawn("chrome")
+    end, { description = "open chrome", group = "launcher" }),
 
     -- volume control
     awful.key({}, "XF86AudioRaiseVolume", function()
@@ -624,3 +609,4 @@ client.connect_signal("unfocus", function(c)
     c.border_color = beautiful.border_normal
 end)
 -- }}}
+
