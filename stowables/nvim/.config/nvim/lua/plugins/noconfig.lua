c3b375d6806fff580b5a379e4ba2dd5de326ebb6
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-01-11 21:18:16 +0800

    fix: revert new PR in typescript-tools, server is only attached on first buffer

--- stowables/nvim/.config/nvim/lua/plugins/noconfig.lua
+++ stowables/nvim/.config/nvim/lua/plugins/noconfig.lua
@@ -5,6 +5,9 @@ return {
     "mfussenegger/nvim-jdtls",
     "simrat39/rust-tools.nvim",
     "b0o/schemastore.nvim",
-    "pmizio/typescript-tools.nvim",
+    {
+        "pmizio/typescript-tools.nvim",
+        commit = "13d9dda",
+    },
     "dmmulroy/tsc.nvim",
 }
