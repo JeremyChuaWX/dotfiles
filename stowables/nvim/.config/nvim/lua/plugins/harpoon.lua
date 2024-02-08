13a481d952876e56bf03bf5c6837c6d2938575c1
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-02-08 19:01:35 +0800

    feat: harpoon mappings

--- stowables/nvim/.config/nvim/lua/plugins/harpoon.lua
+++ stowables/nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -3,26 +3,23 @@ local M = {
     branch = "harpoon2",
     config = function()
         local harpoon = require("harpoon")
-        harpoon:setup()
-
+        harpoon:setup({
+            settings = {
+                save_on_toggle = true,
+                sync_on_ui_close = true,
+            },
+        })
         vim.keymap.set("n", "<leader>hh", function()
             harpoon.ui:toggle_quick_menu(harpoon:list())
         end)
         vim.keymap.set("n", "<leader>ha", function()
             harpoon:list():append()
         end)
-        vim.keymap.set("n", "<leader>1", function()
-            harpoon:list():select(1)
-        end)
-        vim.keymap.set("n", "<leader>2", function()
-            harpoon:list():select(2)
-        end)
-        vim.keymap.set("n", "<leader>3", function()
-            harpoon:list():select(3)
-        end)
-        vim.keymap.set("n", "<leader>4", function()
-            harpoon:list():select(4)
-        end)
+        for i = 1, 9 do
+            vim.keymap.set("n", "<leader>h" .. i, function()
+                harpoon:list():select(i)
+            end)
+        end
     end,
 }
 
