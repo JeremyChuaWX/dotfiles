2d72b0d04e5f0b224a31d1357426f1fb31bc9a04
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-03-09 14:56:43 +0800

    feat: add TODO comments highlighting

---
+++ stowables/nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,16 @@
+local M = {
+    "folke/todo-comments.nvim",
+    config = function()
+        require("todo-comments").setup({
+            signs = false,
+        })
+        vim.keymap.set("n", "]t", function()
+            require("todo-comments").jump_next()
+        end, { desc = "next TODO comment" })
+        vim.keymap.set("n", "[t", function()
+            require("todo-comments").jump_prev()
+        end, { desc = "next TODO comment" })
+    end,
+}
+
+return M
