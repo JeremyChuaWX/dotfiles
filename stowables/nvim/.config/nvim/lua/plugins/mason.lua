535285bdf27af2ba759350d5f40498aa18dffdd2
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-06-05 09:13:21 +0800

    refactor: split mason and lspconfig

---
+++ stowables/nvim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,94 @@
+local mason = {
+    "williamboman/mason.nvim",
+    config = true,
+}
+
+local mason_lspconfig = {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+        local mason_lspconfig = require("mason-lspconfig")
+
+        mason_lspconfig.setup({
+            ensure_installed = {
+                "lua_ls",
+                "tsserver",
+                "jsonls",
+                "tailwindcss",
+                "gopls",
+                "ruff",
+                "pyright",
+            },
+        })
+
+        local lspconfig = require("lspconfig")
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+        mason_lspconfig.setup_handlers({
+            function(server_name)
+                local opts = {
+                    capabilities = capabilities,
+                }
+                local ok, server_opts = pcall(require, "lsp-servers." .. server_name)
+                if ok then
+                    opts = vim.tbl_deep_extend("keep", server_opts, opts)
+                end
+                lspconfig[server_name].setup(opts)
+            end,
+
+            ["jdtls"] = function() end,
+
+            ["rust_analyzer"] = function()
+                vim.g.rustaceanvim = {
+                    server = {
+                        capabilities = capabilities,
+                    },
+                }
+            end,
+
+            ["tsserver"] = function()
+                require("typescript-tools").setup({
+                    capabilities = capabilities,
+                    settings = {
+                        expose_as_code_action = "all",
+                        tsserver_file_preferences = {
+                            providePrefixAndSuffixTextForRename = false,
+                        },
+                    },
+                    handlers = {
+                        ["textDocument/publishDiagnostics"] = function(...)
+                            require("ts-error-translator").translate_diagnostics(...)
+                            vim.lsp.diagnostic.on_publish_diagnostics(...)
+                        end,
+                    },
+                })
+                require("tsc").setup({
+                    auto_focus_qflist = true,
+                })
+            end,
+        })
+    end,
+}
+
+local mason_tools = {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
+        ensure_installed = {
+            "prettier",
+            "mdformat",
+            "stylua",
+            "beautysh",
+            "rustywind",
+            "gofumpt",
+            "golines",
+            "golangci-lint",
+        },
+    },
+}
+
+local M = {
+    mason,
+    mason_lspconfig,
+    mason_tools,
+}
+
+return M
