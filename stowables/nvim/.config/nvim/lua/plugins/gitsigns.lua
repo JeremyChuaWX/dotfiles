d98081ebe54d90bb550419342368a78cf189acc1
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-06-18 15:29:54 +0800

    feat: gitsigns config

--- stowables/nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ stowables/nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,20 +1,8 @@
 return {
     "lewis6991/gitsigns.nvim",
     opts = {
-        signs = {
-            add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-            change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-            delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-            topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-            changedelete = {
-                hl = "GitSignsChange",
-                text = "~",
-                numhl = "GitSignsChangeNr",
-                linehl = "GitSignsChangeLn",
-            },
-        },
         on_attach = function(bufnr)
-            local gs = package.loaded.gitsigns
+            local gitsigns = require("gitsigns")
 
             local function map(mode, l, r, opts)
                 opts = opts or {}
@@ -22,33 +10,33 @@ return {
                 vim.keymap.set(mode, l, r, opts)
             end
 
-            -- Navigation
             map("n", "]g", function()
                 if vim.wo.diff then
-                    return "]g"
+                    vim.cmd.normal({ "]g", bang = true })
+                else
+                    gitsigns.nav_hunk("next")
                 end
-                vim.schedule(function()
-                    gs.next_hunk()
-                end)
-                return "<Ignore>"
             end, { desc = "gitsigns next hunk", expr = true })
 
             map("n", "[g", function()
                 if vim.wo.diff then
-                    return "[g"
+                    vim.cmd.normal({ "]g", bang = true })
+                else
+                    gitsigns.nav_hunk("prev")
                 end
-                vim.schedule(function()
-                    gs.prev_hunk()
-                end)
-                return "<Ignore>"
             end, { desc = "gitsigns prev hunk", expr = true })
 
-            -- Actions
-            map("n", "<leader>gp", gs.preview_hunk, { desc = "gitsigns preview hunk" })
-            map("n", "<leader>gs", gs.stage_hunk, { desc = "gitsigns stage hunk" })
-            map("n", "<leader>gS", gs.stage_buffer, { desc = "gitsigns stage buffer" })
-            map("n", "<leader>gr", gs.reset_hunk, { desc = "gitsigns reset hunk" })
-            map("n", "<leader>gR", gs.reset_buffer, { desc = "gitsigns reset buffer" })
+            map("n", "<leader>gp", gitsigns.preview_hunk, { desc = "gitsigns preview hunk" })
+            map("n", "<leader>gs", gitsigns.stage_hunk, { desc = "gitsigns stage hunk" })
+            map("v", "<leader>gs", function()
+                gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+            end, { desc = "visual gitsigns stage hunk" })
+            map("n", "<leader>gS", gitsigns.stage_buffer, { desc = "gitsigns stage buffer" })
+            map("n", "<leader>gr", gitsigns.reset_hunk, { desc = "gitsigns reset hunk" })
+            map("v", "<leader>gr", function()
+                gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+            end, { desc = "visual gitsigns stage hunk" })
+            map("n", "<leader>gR", gitsigns.reset_buffer, { desc = "gitsigns reset buffer" })
         end,
     },
 }
