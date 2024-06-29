726dda08a32df0bf53d9d2d06d021130d6d8a490
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-06-29 09:26:01 +0800

    feat: update conform options

--- stowables/nvim/.config/nvim/lua/plugins/conform.lua
+++ stowables/nvim/.config/nvim/lua/plugins/conform.lua
@@ -6,7 +6,7 @@ return {
             function()
                 require("conform").format({
                     async = true,
-                    lsp_fallback = true,
+                    lsp_format = "fallback",
                 })
             end,
             desc = "conform format",
