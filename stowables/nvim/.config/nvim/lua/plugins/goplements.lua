c3912decb4dbfb398161b80e8375d4dd1db4d804
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-08-08 16:57:21 +0800

    feat: goplements plugin

---
+++ stowables/nvim/.config/nvim/lua/plugins/goplements.lua
@@ -0,0 +1,11 @@
+return {
+    "maxandron/goplements.nvim",
+    ft = "go",
+    opts = {
+        prefix = {
+            interface = "structs: ",
+            struct = "interfaces: ",
+        },
+        display_package = true,
+    },
+}
