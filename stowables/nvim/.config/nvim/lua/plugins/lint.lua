79f829b4db9b1e3a5eaed4445f7b669ee68d00a9
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-08-31 07:42:10 +0800

    feat: ignore errors in linter

--- stowables/nvim/.config/nvim/lua/plugins/lint.lua
+++ stowables/nvim/.config/nvim/lua/plugins/lint.lua
@@ -11,7 +11,7 @@ return {
         }
         vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter" }, {
             callback = function()
-                require("lint").try_lint()
+                require("lint").try_lint(nil, { ignore_errors = true })
             end,
         })
     end,
