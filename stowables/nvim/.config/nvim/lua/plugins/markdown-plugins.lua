4346694cd61f7f9bec15ce7897d28a33f0842f5b
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-09-23 23:13:33 +0800

    feat: markdown preview

--- stowables/nvim/.config/nvim/lua/plugins/markdown-plugins.lua
+++ stowables/nvim/.config/nvim/lua/plugins/markdown-plugins.lua
@@ -3,19 +3,7 @@ return {
         "bullets-vim/bullets.vim",
     },
     {
-        "toppair/peek.nvim",
-        event = { "VeryLazy" },
-        build = "deno task --quiet build:fast",
-        config = function()
-            require("peek").setup()
-            vim.api.nvim_create_user_command("Peek", function()
-                local peek = require("peek")
-                if peek.is_open() then
-                    peek.close()
-                else
-                    peek.open()
-                end
-            end, {})
-        end,
+        "brianhuster/live-preview.nvim",
+        config = true,
     },
 }
