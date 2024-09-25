837ae490bc1181643dd4c117fef0048977b4a7d1
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-09-25 09:24:41 +0800

    refactor: remove local M

--- stowables/nvim/.config/nvim/lua/config/colorscheme.lua
+++ stowables/nvim/.config/nvim/lua/config/colorscheme.lua
@@ -1,11 +1,9 @@
-local M = {}
-
-M.set_cs = function(name)
-    local cs_ok, _ = pcall(vim.cmd.colorscheme, name)
-    if not cs_ok then
-        vim.notify("colorscheme " .. name .. " not found!")
-        return
-    end
-end
-
-return M
+return {
+    set_cs = function(name)
+        local cs_ok, _ = pcall(vim.cmd.colorscheme, name)
+        if not cs_ok then
+            vim.notify("colorscheme " .. name .. " not found!")
+            return
+        end
+    end,
+}
