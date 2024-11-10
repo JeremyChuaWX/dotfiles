c6ee6332e5a474d9cd82cd5110d808348453766d
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-11-10 10:43:21 +0800

    feat: update bullet heirarchies

--- stowables/nvim/.config/nvim/lua/plugins/markdown-plugins.lua
+++ stowables/nvim/.config/nvim/lua/plugins/markdown-plugins.lua
@@ -1,6 +1,9 @@
 return {
     {
         "bullets-vim/bullets.vim",
+        config = function()
+            vim.g.bullets_outline_levels = { "ROM", "ABC", "num", "abc", "rom", "std-" }
+        end,
     },
     {
         "brianhuster/live-preview.nvim",
