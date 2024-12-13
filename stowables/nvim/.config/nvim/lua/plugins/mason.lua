6b88f51fdcb60e821477ef478b054f3dc739e893
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-12-13 22:03:00 +0100

    feat: lsp capabilities

--- stowables/nvim/.config/nvim/lua/plugins/mason.lua
+++ stowables/nvim/.config/nvim/lua/plugins/mason.lua
@@ -22,7 +22,11 @@ local mason_lspconfig = {
         })
 
         local lspconfig = require("lspconfig")
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            vim.lsp.protocol.make_client_capabilities(),
+            require("cmp_nvim_lsp").default_capabilities()
+        )
 
         mason_lspconfig.setup_handlers({
             function(server_name)
