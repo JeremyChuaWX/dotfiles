0a8c3537c1a3b56c5e1ad16660e80a11ad010040
Jeremy Chua <chua.jeremy.wx@gmail.com>
2024-12-31 11:53:45 +0800

    feat: check if brew is installed

--- macos/macos_setup.sh
+++ macos/macos_setup.sh
@@ -1,31 +1,33 @@
 #!/bin/sh
 
-echo "Applying OSX settings"
+echo "applying OSX settings"
 source osx_settings
 
-echo "Installing homebrew"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [[ $(command -v brew) == "" ]]; then
+    echo "installing homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    export PATH=/opt/homebrew/bin:$PATH
+else
+    echo "homebrew installed"
+fi
 
-echo "Add homebrew to path"
-export PATH=/opt/homebrew/bin:$PATH
-
-echo "Installing brew bundle"
+echo "installing brew bundle"
 brew bundle --file="./brewfile"
 
-echo "Logging in to github CLI"
+echo "logging in to github CLI"
 gh auth login
 
-echo "Installing npm packages"
+echo "installing npm packages"
 source npmfile
 
-echo "Stowing configs"
+echo "stowing configs"
 cd $HOME/.dotfiles/stowables
 stow -vR */ -t ~
 
-echo "Install tmux terminfo"
+echo "install tmux terminfo"
 cd $HOME
 curl -LO https://invisible-island.net/datafiles/current/terminfo.src.gz && gunzip terminfo.src.gz
 /usr/bin/tic -xe tmux-256color terminfo.src
 
-echo "Link .profile from icloud to home"
+echo "link .profile from icloud to home"
 ln -s "$HOME/Library/Mobile Documents/com~apple~CloudDocs/.profile" "$HOME/.profile"
