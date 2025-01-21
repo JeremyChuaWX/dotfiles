ae1bc4facfb79dd82eef6be674896719f561afa8
Jeremy Chua <chua.jeremy.wx@gmail.com>
2025-01-21 17:03:11 +0800

    feat: oil-git-status

--- stowables/nvim/.config/nvim/lua/plugins/oil.lua
+++ stowables/nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,23 +1,32 @@
 return {
-    "stevearc/oil.nvim",
-    keys = {
-        {
-            "-",
-            function()
-                require("oil").open_float()
-            end,
-            "oil open",
+    {
+        "stevearc/oil.nvim",
+        keys = {
+            {
+                "-",
+                function()
+                    require("oil").open_float()
+                end,
+                "oil open",
+            },
+        },
+        opts = {
+            keymaps = {
+                ["q"] = "actions.close",
+            },
+            view_options = {
+                show_hidden = true,
+            },
+            win_options = {
+                signcolumn = "yes:2",
+            },
+            float = {
+                border = "none",
+            },
         },
     },
-    opts = {
-        keymaps = {
-            ["q"] = "actions.close",
-        },
-        view_options = {
-            show_hidden = true,
-        },
-        float = {
-            border = "none",
-        },
+    {
+        "refractalize/oil-git-status.nvim",
+        config = true,
     },
 }
