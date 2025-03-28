3eb58dd47398206d828238b3446eb3bfe8e4c193
Jeremy Chua <chua.jeremy.wx@gmail.com>
2025-03-28 08:30:49 +0800

    refactor: move to separate lsp file

--- stowables/nvim/.config/nvim/lua/plugins/noconfig.lua
+++ stowables/nvim/.config/nvim/lua/plugins/noconfig.lua
@@ -1,9 +1,10 @@
 return {
-    "nvim-lua/popup.nvim",
-    "nvim-lua/plenary.nvim",
-    "mfussenegger/nvim-jdtls",
     "b0o/schemastore.nvim",
-    "pmizio/typescript-tools.nvim",
     "dmmulroy/ts-error-translator.nvim",
+    "mfussenegger/nvim-jdtls",
+    "neovim/nvim-lspconfig",
+    "nvim-lua/plenary.nvim",
+    "nvim-lua/popup.nvim",
+    "pmizio/typescript-tools.nvim",
     "ray-x/lsp_signature.nvim",
 }
