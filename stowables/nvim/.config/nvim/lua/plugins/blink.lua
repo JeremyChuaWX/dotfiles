e7682e584a5f4b951347c94bc9b77e6a5d9dff44
Jeremy Chua <chua.jeremy.wx@gmail.com>
2025-07-13 10:33:44 +0800

    feat: update blink config

--- stowables/nvim/.config/nvim/lua/plugins/blink.lua
+++ stowables/nvim/.config/nvim/lua/plugins/blink.lua
@@ -2,57 +2,54 @@ return {
     "saghen/blink.cmp",
     dependencies = {
         "rafamadriz/friendly-snippets",
-        "onsails/lspkind.nvim",
     },
     version = "1.*",
 
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-        keymap = { preset = "default" },
+        keymap = {
+            preset = "none",
+            ["<C-y>"] = { "show" },
+            ["<C-e>"] = { "hide" },
+            ["<C-b>"] = { "scroll_documentation_up" },
+            ["<C-f>"] = { "scroll_documentation_down" },
+            ["<C-p>"] = { "select_prev" },
+            ["<C-n>"] = { "select_next" },
+            ["<C-k>"] = { "snippet_backward" },
+            ["<C-l>"] = { "snippet_forward" },
+            ["<CR>"] = { "select_and_accept", "fallback" },
+        },
         appearance = {
             nerd_font_variant = "mono",
         },
         completion = {
-            documentation = { auto_show = false },
-            menu = {
-                draw = {
-                    components = {
-                        kind_icon = {
-                            text = function(ctx)
-                                local icon = ctx.kind_icon
-                                if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                                    local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-                                    if dev_icon then
-                                        icon = dev_icon
-                                    end
-                                else
-                                    icon = require("lspkind").symbolic(ctx.kind, {
-                                        mode = "symbol",
-                                    })
-                                end
-
-                                return icon .. ctx.icon_gap
-                            end,
-                            highlight = function(ctx)
-                                local hl = ctx.kind_hl
-                                if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                                    local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
-                                    if dev_icon then
-                                        hl = dev_hl
-                                    end
-                                end
-                                return hl
-                            end,
-                        },
+            documentation = {
+                auto_show = false,
+            },
+        },
+        sources = {
+            default = { "lsp", "snippets", "path", "buffer" },
+        },
+        fuzzy = {
+            implementation = "prefer_rust_with_warning",
+        },
+        cmdline = {
+            keymap = {
+                preset = "inherit",
+                ["<CR>"] = { "accept_and_enter", "fallback" },
+            },
+            completion = {
+                menu = {
+                    auto_show = true,
+                },
+                list = {
+                    selection = {
+                        preselect = false,
                     },
                 },
             },
         },
-        sources = {
-            default = { "lsp", "path", "snippets", "buffer" },
-        },
-        fuzzy = { implementation = "prefer_rust_with_warning" },
     },
     opts_extend = { "sources.default" },
 }
