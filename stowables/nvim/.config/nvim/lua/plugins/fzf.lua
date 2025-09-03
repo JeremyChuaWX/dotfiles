df7066c2a103751996c0f958e3a823b9f9e16a3a
Jeremy Chua <chua.jeremy.wx@gmail.com>
2025-09-04 00:04:15 +0800

    feat: try fzf-lua

---
+++ stowables/nvim/.config/nvim/lua/plugins/fzf.lua
@@ -0,0 +1,28 @@
+return {
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+        {
+            "<leader>ff",
+            function()
+                require("fzf-lua").files()
+            end,
+            desc = "fzf-lua files",
+        },
+        {
+            "<leader>fg",
+            function()
+                require("fzf-lua").grep_project()
+            end,
+            desc = "fzf-lua live grep",
+        },
+        {
+            "<leader>fb",
+            function()
+                require("fzf-lua").buffers()
+            end,
+            desc = "fzf-lua buffers",
+        },
+    },
+    opts = {},
+}
