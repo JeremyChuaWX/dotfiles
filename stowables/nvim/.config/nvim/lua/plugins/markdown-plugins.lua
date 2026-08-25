8d5ddc5d7ac08a3884f86544a6de000529772d49
Jeremy Chua <chua.jeremy.wx@gmail.com>
2026-08-25 23:35:04 +0800

    feat: bullets remap

--- stowables/nvim/.config/nvim/lua/plugins/markdown-plugins.lua
+++ stowables/nvim/.config/nvim/lua/plugins/markdown-plugins.lua
@@ -4,6 +4,21 @@ return {
         init = function()
             vim.g.bullets_outline_levels = { "ROM", "ABC", "num", "abc", "rom", "std-" }
         end,
+        config = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "markdown",
+                callback = function(args)
+                    vim.keymap.set("x", "<", "<Plug>(bullets-promote)gv", {
+                        buffer = args.buf,
+                        remap = true,
+                    })
+                    vim.keymap.set("x", ">", "<Plug>(bullets-demote)gv", {
+                        buffer = args.buf,
+                        remap = true,
+                    })
+                end,
+            })
+        end,
     },
     {
         "brianhuster/live-preview.nvim",
